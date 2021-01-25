a0dcc379c1f13ba310b03e08a3d5e419c642b077
lunaceee <lunaceee@gmail.com>
2021-01-25 10:20:21 -0800

    install jest fetch mock plugin

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,8 @@
 import "@testing-library/jest-dom";
 import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import fetchMock from "jest-fetch-mock";
+
+fetchMock.enableMocks();
 
 configure({ adapter: new Adapter() });
